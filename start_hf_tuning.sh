f7b8085064d1f0e610dd4cc2314143eeec541b2d
jboistel <jboistel@gmail.com>
2026-03-01 10:42:18 +0100

    add lib to hf call

--- start_hf_tuning.sh
+++ start_hf_tuning.sh
@@ -1,8 +1,12 @@
 hf jobs uv run \
-    --flavor cpu-basic \
+    --flavor l4x1 \
     --with trl \
     --with transformers \
     --with peft \
     --with datasets \
+    --with dotenv \
+    --with liger-kernel \
+    --with bitsandbytes \
     --secrets HF_TOKEN \
+    --secrets DATASET_NAME \
     fine_tuning.py
